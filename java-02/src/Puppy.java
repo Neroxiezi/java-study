de6027d7447072fd535c8f2ebb26a8dbc4b46055
南丞 <lampxiezi@163.com>
2019-05-07 14:00:11 +0800

    更新

--- java-02/src/Puppy.java
+++ java-02/src/Puppy.java
@@ -5,5 +5,21 @@ public class Puppy {
     public Puppy(String name) {
         System.out.println("找到的动物:" + name);
     }
-    
+
+    public void setAge(int age) {
+        puppyAge = age;
+    }
+
+    public int getAge() {
+        System.out.println("动物的年龄:" + puppyAge);
+        return puppyAge;
+    }
+
+    public static void main(String[] args) {
+        Puppy obj = new Puppy("小猫");
+        obj.setAge(18);
+        obj.getAge();
+        System.out.println("属性值:" + obj.puppyAge);
+    }
+
 }
