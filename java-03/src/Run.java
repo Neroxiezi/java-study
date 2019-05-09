05b279ed81d604053ef2d26fcad841a406a9d7bc
南丞 <lampxiezi@163.com>
2019-05-09 17:52:44 +0800

    更新

---
+++ java-03/src/Run.java
@@ -0,0 +1,38 @@
+/**
+ * @Author: 南丞
+ * @Email: lampxiezi@163.com
+ * @Date:Created in 17:45 2019/5/9
+ * @Description: I don't know why to write like this.
+ * .------..------..------.
+ * |B.--. ||U.--. ||G.--. |
+ * | :(): || (\/) || :/\: |
+ * | ()() || :\/: || :\/: |
+ * | '--'B|| '--'U|| '--'G|
+ * `------'`------'`------'
+ */
+
+class MyThread extends Thread {
+    private int count = 5;
+
+    public MyThread(String name) {
+        super();
+        this.setName(name);
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        while (count >0) {
+            count--;
+            System.out.println("由 " + MyThread.currentThread().getName() + "计算,count= " + count);
+        }
+    }
+}
+
+public class Run {
+    public static void main(String[] args) {
+        MyThread a = new MyThread("A");
+        MyThread b = new MyThread("B");
+
+    }
+}
